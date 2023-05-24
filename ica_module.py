328cb2ef37a0279a4883fff126dee46cf9e046ce
mirhnius <niushamirhakimi@gmail.com>
2023-05-23 21:57:16 -0400

    add weighted sum function

--- ica_module.py
+++ ica_module.py
@@ -1,3 +1,4 @@
+import copy
 import pathlib
 import numpy as np
 from nilearn.image import binarize_img, new_img_like, iter_img, threshold_img
@@ -38,9 +39,40 @@ def ICA_decomposition(filenames, group, i, path=ICS_PATH, n=n_components):
     ICA_s.to_filename(path / (f'ICAs_{group}_{i}.nii.gz'))
     return ICA_s
 
+def threshold_masking(data, mask, threshold="99.6%", two_sided=False):
+    
+    mask_thr = threshold_img(img=mask, threshold=threshold, two_sided=two_sided)
+    mask_d = mask_thr.get_fdata()
+    masked_data = data * np.reshape(mask_d, newshape=list(mask_d.shape)+[1])
+    
+    return masked_data, mask_thr
 
+def mean_calc(extracted_from_mask, masked_data, i):
+    
+    n_subjects = extracted_from_mask.shape[1]
+    for j in range(n_subjects):
+            subject = masked_data[...,j]
+            index = np.nonzero(subject)
+            extracted_from_mask[i,j] = np.nanmean(subject[index])
+    
+    return extracted_from_mask
 
-def Means_after_masking(ICAs,DBM_maps,path=NonePath,group=""):
+def weighted_sum_masking(data, mask):
+    
+    mask_d = copy.deepcopy(mask.get_fdata())
+    mask_d[mask_d < 0] = 0
+    masked_data = data * np.reshape(mask_d, newshape=list(mask_d.shape)+[1])
+    filtered_mask = new_img_like(ref_niimg=mask, data=mask_d)
+    
+    return masked_data, filtered_mask
+
+def whighted_sum_calc(extracted_from_mask, masked_data, mask_d, i):
+
+    extracted_from_mask[i,:] = np.sum(masked_data, axis=(0,1,2)) / np.sum(mask_d)
+    
+    return extracted_from_mask
+
+def Means_after_masking(ICAs, DBM_maps, path=NonePath, group="", func=threshold_masking):
     """
     This function first extracts a mask from each IC and then applies it to each subject.
     After extracting regions of interest, the function calculates the mean value of these regions
@@ -60,26 +92,24 @@ def Means_after_masking(ICAs,DBM_maps,path=NonePath,group=""):
     if path.resolve()is not None:
       mask_dir.mkdir(parents=True, exist_ok=True)  
       masked_data_dir.mkdir(parents=True, exist_ok=True)
-
+      
     n = ICAs.shape[3]
-    n_subjects = DBM_maps.shape[3]
-    means_after_mask = np.zeros((n,n_subjects), dtype='<f8') #, dtype='<f4'
+    n_subjects = data.shape[3]
+    extracted_from_mask = np.zeros((n,n_subjects), dtype='<f8') #, dtype='<f4'
     data = DBM_maps.get_fdata()
 
     for i, cur_ic in enumerate(iter_img(ICAs)):
 
-        mask = threshold_img(img=cur_ic, threshold="99.6%", two_sided=False)
-        mask_d = mask.get_fdata()
-        masked_data = data * np.reshape(mask_d, newshape=list(mask_d.shape)+[1])
-
-        for j in range(n_subjects):
-            subject = masked_data[...,j]
-            index = np.nonzero(subject)
-            means_after_mask[i,j] = np.nanmean(subject[index])
-
+        masked_data, mask = func(data, cur_ic)
+        
+        if str(func) == "threshold_masking":
+            whighted_sum_calc(extracted_from_mask, masked_data, mask.get_fdata(), i)
+        elif  str(func) == "weighted_sum_masking": 
+            extracted_from_mask = whighted_sum_calc(extracted_from_mask, masked_data, mask.get_fdata(), i)
+        
         if path.resolve() is not None:
             # new_img_like(DBM_maps, masked_data).to_filename(masked_data_dir / f"masked_{group}_IC_{i}.nii.gz")
-            np.savetxt(masked_data_dir / f"masked_{group}_IC_{i}.txt", means_after_mask)
+            np.savetxt(masked_data_dir / f"masked_{group}_IC_{str(func)}_{i}.txt", extracted_from_mask)
             mask.to_filename(mask_dir / f"IC_{group}_{i}.nii.gz")
 
-    return means_after_mask
+    return extracted_from_mask
